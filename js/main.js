46f09f5d3f62451f89166228ef78ae02990b6127
Justine <tinejus.vostpro@gmail.com>
2020-08-29 16:20:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,80 @@
-/* =================
+gsap.registerPlugin(ScrollTrigger);
 
-author: Karan Mhatre
-email: me@karanmhatre.com
-website: karanmhatre.com
-  
-================= */
+barba.init({
+	sync: true,
 
-// Function to add and remove the page transition screen
+	transitions: [
+		{
+			async leave(data) {
+				console.log(data);
+				pageChange();
+				await delay(1000);
+				data.current.container.remove();
+			},
+
+
+			async enter(data) {	
+				// In the next page
+				allAnimationComponents();
+                $(window).scrollTop(0);
+			},
+
+			async once(data) {
+				allAnimationComponents();
+			},
+		},
+	],
+});
+
+function allAnimationComponents() {
+	bannerAnimation();
+	contentAnimation();
+}
+
+function bannerAnimation() {
+	const bannerTL = gsap.timeline();
+	bannerTL
+		
+		.from("#first-appear", {
+			delay: 0.3,
+			y: 25,
+			opacity: 0,
+			duration: 0.5,
+			ease: "power3.inOut"
+		})
+        .from(".second-appear", {
+			delay: 0.5,
+			y: 25,
+			opacity: 0,
+			duration: 0.5,
+			ease: "power3.inOut"
+		})
+
+}
+
+function contentAnimation() {
+
+	console.log('content animation');
+
+	var contentTl = gsap.timeline({
+		scrollTrigger: {
+			trigger:".section-c",
+			markers:true,
+			start:"top 85%", 
+			end:"bottom 60%",
+			toggleActions:"play none none none"
+		},
+	});
+
+	contentTl.addLabel("start").fromTo(".about-content .title", { y: 16,opacity: 0, }, { opacity: 1, y: 0, duration: 0.5, ease: "power3.inOut" })
+    .fromTo(".boxes", { y:50, opacity: 0, }, { opacity: 1, y: 0, duration: 1, ease: "power3.Out",stagger: { amount: 0.2 } })
+	.addLabel("end")
+	.fromTo(".about-content h4", { y: 40, opacity: 0, }, { opacity: 1, y: 0, duration: 0.6, ease: "power3.inOut" })
+	.fromTo(".content-list", { y: 40, opacity: 0, }, { opacity: 1, y: 0, duration: 0.3, ease: "power3.inOut",stagger: { amount: 0.2 } })
+	.addLabel("end")
+    ;
+}
+
 function pageChange() {
   var tl = gsap.timeline();
   tl.set('.loading-screen', { transformOrigin: "bottom left"});
@@ -14,39 +82,12 @@ function pageChange() {
   tl.to('.loading-screen', { duration: 0.5, scaleY: 0, skewX: 0, transformOrigin: "top left", ease: "power1.out", delay: 1 });
 }
 
-// Function to animate the content of each page
-function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from('.is-animated', { duration: 0.5, translateY: 10, opacity: 0 });
-    tl.from('.main-navigation', { duration: 0.5, translateY: -10, opacity: 0, delay: 1});
-    tl.from('.left', {duration:0.5, translateY: 10, opacity:0});    
+
+function delay(n) {
+	n = n || 2000;
+	return new Promise((done) => {
+		setTimeout(() => {
+			done();
+		}, n);
+	});
 }
-
-
-$(function() {
-
-  barba.init({
-    sync: true,
-    transitions: [{
-        async once (data) {
-        contentAnimation();
-      },
-        
-        async after (data) {
-                pageChange();
-        },
-        
-        async leave (data) {
-        const done = this.async();
-        pageChange();
-        contentAnimation();
-        await delay(1000);
-        $(window).scrollTop(0);
-        done();
-      },
-
-    }]
-  });
-    
-
-});
